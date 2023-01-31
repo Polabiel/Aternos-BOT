44d73fc61c22dfc385270c27724a55d9f0debf59
Polabiel <bielgabrieloliveira77@gmail.com>
2023-01-31 20:08:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const mineflayer = require("mineflayer");
+const pvp = require("mineflayer-pvp").plugin;
+const { pathfinder } = require("mineflayer-pathfinder");
+const armorManager = require("mineflayer-armor-manager");
+const AutoAuth = require("mineflayer-auto-auth");
+
+function createBot() {
+  const nicknames = [
+    "bot",
+    "escreveaqui",
+    "aternos",
+    "chaco-bot",
+    "passa no canal do polabiel",
+    "minecraft4d"
+  ];
+  const getContent = nicknames[Math.floor(Math.random() * nicknames.length)];
+
+  const bot = mineflayer.createBot({
+    viewDistance: 1,
+    host: "coloca o IP aqui", // <= Coloca seu IP aqui
+    version: "1.17",
+    username: `${getContent}`,
+    port: 24466, // <= Coloca sua PORTA aqui
+    plugins: [AutoAuth],
+    AutoAuth: "bot112022",
+  });
+  bot.loadPlugin(pvp);
+  bot.loadPlugin(armorManager);
+  bot.loadPlugin(pathfinder);
+
+// ------------ ------------ Não venha mexer aqui ------------ -----------
+  
+  bot.on("playerCollect", (collector) => {
+    if (collector !== bot.entity) return;
+
+    setTimeout(() => {
+      const sword = bot.inventory
+        .items()
+        .find((item) => item.name.includes("sword"));
+      if (sword) bot.equip(sword, "hand");
+    }, 150);
+  });
+
+  bot.on("playerCollect", (collector) => {
+    if (collector !== bot.entity) return;
+
+    setTimeout(() => {
+      const shield = bot.inventory
+        .items()
+        .find((item) => item.name.includes("shield"));
+      if (shield) bot.equip(shield, "off-hand");
+    }, 250);
+  });
+
+  let guardPos = null;
+
+  bot.on("physicTick", () => {
+    if (bot.pvp.target) return;
+    if (bot.pathfinder.isMoving()) return;
+
+    const entity = bot.nearestEntity();
+    if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0));
+  });
+
+  bot.on("physicTick", () => {
+    if (!guardPos) return;
+    const filter = (e) =>
+      e.type === "mob" &&
+      e.position.distanceTo(bot.entity.position) < 16 &&
+      e.mobType !== "Armor Stand";
+    const entity = bot.nearestEntity(filter);
+    if (entity) {
+      bot.pvp.attack(entity);
+    }
+  });
+
+  bot.on("kicked", console.log('❌ O bot foi Kickado'),console.log);
+  bot.on("error", console.log('❌ Deu erro aqui'), console.log);
+  bot.on("end", createBot);
+
+  bot.on("time", () => {});
+}
+
+createBot();
+console.log("💀 Foi iniciado o BOT 💀");
---
+++ squarecloud.app
@@ -0,0 +1,6 @@
+DISPLAY_NAME=Aternos AFK-BOT
+DESCRIPTION= Fica AFK no servidores da Aternos.
+MAIN=index.js
+AVATAR=https://media.discordapp.net/attachments/1041367858531283026/1063016961841307678/b43d99a24c5309ee8a3b17e7828990b7.png
+MEMORY=100
+VERSION=recommended
